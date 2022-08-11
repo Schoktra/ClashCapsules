b66b196deb905ddb2c14f766ae44551092968f39
Schoktra <schok@192.168.0.16>
2022-08-11 10:43:28 -0500

    Fixed a minor bug causing the capsule table to delete items placed in the rightmost column of the grid.

--- src/main/java/net/mcreator/mineclash/block/CpasuleCraftingtableBlock.java
+++ src/main/java/net/mcreator/mineclash/block/CpasuleCraftingtableBlock.java
@@ -17,6 +17,7 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.TieredItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.entity.player.Player;
@@ -26,7 +27,6 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.Containers;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.FriendlyByteBuf;
@@ -35,7 +35,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.client.Minecraft;
 
 import net.mcreator.mineclash.world.inventory.CapsueltableoverlayMenu;
-import net.mcreator.mineclash.procedures.ClashCapsuleCraftingtableUpdateTickProcedure;
 import net.mcreator.mineclash.block.entity.CpasuleCraftingtableBlockEntity;
 
 import java.util.Random;
@@ -49,7 +48,7 @@ public class CpasuleCraftingtableBlock extends Block
 
 			EntityBlock {
 	public CpasuleCraftingtableBlock() {
-		super(BlockBehaviour.Properties.of(Material.STONE).sound(SoundType.WOOD).strength(1f, 10f));
+		super(BlockBehaviour.Properties.of(Material.STONE).sound(SoundType.WOOD).strength(1f, 10f).requiresCorrectToolForDrops());
 	}
 
 	@Override
@@ -64,28 +63,18 @@ public class CpasuleCraftingtableBlock extends Block
 	}
 
 	@Override
+	public boolean canHarvestBlock(BlockState state, BlockGetter world, BlockPos pos, Player player) {
+		if (player.getInventory().getSelected().getItem() instanceof TieredItem tieredItem)
+			return tieredItem.getTier().getLevel() >= 1;
+		return false;
+	}
+
+	@Override
 	public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
 		List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 		if (!dropsOriginal.isEmpty())
 			return dropsOriginal;
 		return Collections.singletonList(new ItemStack(this, 1));
-	}
-
-	@Override
-	public void onPlace(BlockState blockstate, Level world, BlockPos pos, BlockState oldState, boolean moving) {
-		super.onPlace(blockstate, world, pos, oldState, moving);
-		world.scheduleTick(pos, this, 10);
-	}
-
-	@Override
-	public void tick(BlockState blockstate, ServerLevel world, BlockPos pos, Random random) {
-		super.tick(blockstate, world, pos, random);
-		int x = pos.getX();
-		int y = pos.getY();
-		int z = pos.getZ();
-
-		ClashCapsuleCraftingtableUpdateTickProcedure.execute(world, x, y, z);
-		world.scheduleTick(pos, this, 10);
 	}
 
 	@OnlyIn(Dist.CLIENT)
