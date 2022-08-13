61982286149dbd034abe2a27b2c75b87fa81fc94
nerdmuffin <keisc_uiav4kv@DESKTOP-00I1QOG>
2022-08-13 17:58:31 +0200

    added "Ukuleli" crafting recipe (failed)

--- src/main/java/net/mcreator/mineclash/procedures/TimeClashCapsuleEffectProcedure.java
+++ src/main/java/net/mcreator/mineclash/procedures/TimeClashCapsuleEffectProcedure.java
@@ -15,7 +15,7 @@ public class TimeClashCapsuleEffectProcedure {
 			return;
 		if (true) {
 			if (entity instanceof LivingEntity _entity)
-				_entity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 600, 99999));
+				_entity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 600, 9999));
 			if (sourceentity instanceof Player _player) {
 				ItemStack _stktoremove = new ItemStack(MineclashModItems.TIME_CLASH_CAPSULE.get());
 				_player.getInventory().clearOrCountMatchingItems(p -> _stktoremove.getItem() == p.getItem(), 1,
