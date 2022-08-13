72dda4a974527920886b3f834176488a6a7b23d2
Schoktra <schok@MSI.mshome.net>
2022-08-13 00:16:26 -0500

    Added Invisibility Capsule Recipe

---
+++ src/main/java/net/mcreator/mineclash/procedures/InvisibleCapsuleRecipeProcedure.java
@@ -0,0 +1,47 @@
+package net.mcreator.mineclash.procedures;
+
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerPlayer;
+
+import net.mcreator.mineclash.init.MineclashModItems;
+
+import java.util.function.Supplier;
+import java.util.Map;
+
+public class InvisibleCapsuleRecipeProcedure {
+	public static void execute(Entity entity) {
+		if (entity == null)
+			return;
+		if ((entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt
+				? ((Slot) _slt.get(0)).getItem()
+				: ItemStack.EMPTY).getItem() == Blocks.GLASS.asItem()
+				&& (entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+						&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(1)).getItem() : ItemStack.EMPTY).getItem() == Blocks.GLASS.asItem()
+				&& (entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+						&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(2)).getItem() : ItemStack.EMPTY).getItem() == Blocks.GLASS.asItem()
+				&& (entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+						&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(3)).getItem() : ItemStack.EMPTY).getItem() == Blocks.GLASS.asItem()
+				&& (entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+						&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(4)).getItem() : ItemStack.EMPTY)
+						.getItem() == MineclashModItems.PLAIN_CLASHCAPSULE.get()
+				&& (entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+						&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(5)).getItem() : ItemStack.EMPTY).getItem() == Blocks.GLASS.asItem()
+				&& (entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+						&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(6)).getItem() : ItemStack.EMPTY).getItem() == Blocks.GLASS.asItem()
+				&& (entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+						&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(7)).getItem() : ItemStack.EMPTY).getItem() == Blocks.GLASS.asItem()
+				&& (entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+						&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(8)).getItem() : ItemStack.EMPTY).getItem() == Blocks.GLASS.asItem()) {
+			if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+					&& _current.get() instanceof Map _slots) {
+				ItemStack _setstack = new ItemStack(MineclashModItems.INVISIBILITY_CLASH_CAPSULE.get());
+				_setstack.setCount(1);
+				((Slot) _slots.get(9)).set(_setstack);
+				_player.containerMenu.broadcastChanges();
+			}
+		}
+	}
+}
